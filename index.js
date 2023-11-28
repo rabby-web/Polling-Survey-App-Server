423f54bcf89c8df8034911574b5b55b96cc10021
rabby-web <rabby.webdeveloper@gmail.com>
2023-11-28 12:56:07 +0600

    admin role add

--- index.js
+++ index.js
@@ -41,30 +41,47 @@ async function run() {
     const verifyToken = (req, res, next) => {
       console.log("inside verify token", req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: "forbidden access" });
+        return res.status(401).send({ message: "unauthorized access" });
       }
       const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: "forbidden access" });
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
         next();
       });
     };
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded?.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+    const verifySurveyor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isSurveyor = user?.role === "surveyor";
+      if (!isSurveyor) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
 
     // user related api
-
-    app.get("/users", async (req, res) => {
+    app.get("/users", verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
     // admin
     app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      console.log("hello");
       const email = req.params.email;
-      if (email !== req.destroyed.email) {
-        return res.status(403).send({ message: "unauthorized access" });
-      }
       const query = { email: email };
       const user = await userCollection.findOne(query);
       let admin = false;
@@ -72,6 +89,17 @@ async function run() {
         admin = user?.role === "admin";
       }
       res.send({ admin });
+    });
+    // Surveyor
+    app.get("/users/surveyor/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let surveyor = false;
+      if (user) {
+        surveyor = user?.role === "surveyor";
+      }
+      res.send({ surveyor });
     });
 
     app.post("/users", async (req, res) => {
